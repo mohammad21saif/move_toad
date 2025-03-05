0dd1e46f21e32242655da728bb7d4393dbd524d1
mohammad21saif <mohammad21@iiserb.ac.in>
2025-03-05 12:41:53 +0530

    motor works

---
+++ test.py
@@ -0,0 +1,28 @@
+from pymavlink import mavutil
+import sys
+
+master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200)
+master.wait_heartbeat()
+
+def set_rc_channel_pwm(channel_id, pwm=1500):
+    """ Set RC channel pwm value
+    Args:
+        channel_id (TYPE): Channel ID
+        pwm (int, optional): Channel pwm value 1100-1900
+    """
+    if channel_id < 1 or channel_id > 18:
+        print("Channel does not exist.")
+        return
+
+    # Mavlink 2 supports up to 18 channels:
+    # https://mavlink.io/en/messages/common.html#RC_CHANNELS_OVERRIDE
+    rc_channel_values = [65535 for _ in range(18)]
+    rc_channel_values[channel_id - 1] = pwm
+    master.mav.rc_channels_override_send(
+        master.target_system,                # target_system
+        master.target_component,             # target_component
+        *rc_channel_values)                  # RC channel list, in microseconds.
+
+
+# Set some roll
+set_rc_channel_pwm(1, 1200)
